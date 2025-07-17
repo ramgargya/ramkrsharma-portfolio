fdeb39f03e39fe400c9a070efaf32a47f1118613
Ram Sharma <ram2213020@akgec.ac.in>
2025-07-18 03:10:32 +0530

    Initial commit

---
+++ src/main/java/in/ramkrsharma/portfolio/PersonalportfolioApplication.java
@@ -0,0 +1,13 @@
+package in.ramkrsharma.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalportfolioApplication.class, args);
+	}
+
+}
